6eba782fa8f2195c5ef6ff873e0d340a5219d2f9
hung132 <hung132132@gmail.com>
2014-10-10 08:48:40 +0700

    create packages

---
+++ src/main/java/com/hung/utility/Test.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.hung.utility;
+
+/**
+ *
+ * @author hung132
+ */
+public class Test {
+    
+}
